3fe1127803939f5b5ea6ab20feb77a82caaa623c
clement <clement.lidar@gmail.com>
2023-03-16 12:45:03 +0100

    redesign plan card

--- src/components/Store/Plan.js
+++ src/components/Store/Plan.js
@@ -7,13 +7,43 @@ const Plan = () => {
         <div className="wrapper">
           <div className="header-plan">
             <h2>
-              <span>10</span> Crédits
+              <span>1</span> Crédit
             </h2>
-            <small>19,99€ par lead</small>
+            <small>99,99€ par lead</small>
           </div>
           <div className="plan-content">
             <p>
-              199,<span> 99 €</span>
+              99,<span> 99 €</span>
+            </p>
+            <small>TTC</small>
+          </div>
+          <button className="btn-confirm">Acheter</button>
+        </div>
+        <div className="wrapper main-plan">
+          <div className="header-plan">
+            <h2>
+              <span>5</span> Crédits
+            </h2>
+            <small>69,99€ par lead</small>
+          </div>
+          <div className="plan-content">
+            <p>
+              345,<span> 99 €</span>
+            </p>
+            <small>TTC</small>
+          </div>
+          <button className="btn-confirm">Acheter</button>
+        </div>
+        <div className="wrapper">
+          <div className="header-plan">
+            <h2>
+              <span>10</span> Crédits
+            </h2>
+            <small>49,99€ par lead</small>
+          </div>
+          <div className="plan-content">
+            <p>
+              499,<span> 99 €</span>
             </p>
             <small>TTC</small>
           </div>
