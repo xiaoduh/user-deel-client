879c0c3da48374d942e998f22142a5469d878cc0
ClémentL <clement.lidar@gmail.com>
2023-04-11 22:45:56 +0200

    import thunk from "redux-thunk";

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from "./App";
 import "./style/index.scss";
 import { Provider } from "react-redux";
 import { applyMiddleware, createStore } from "redux";
+import thunk from "redux-thunk";
 // dev tools
 import rootReducer from "./reducers";
 import { composeWithDevTools } from "redux-devtools-extension";
