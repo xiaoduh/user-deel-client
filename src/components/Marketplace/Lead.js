ba00947f4b14b7bff019d6a6ac8ade004ef29d71
ClémentL <clement.lidar@gmail.com>
2023-04-12 22:36:32 +0200

    <th>Statut</th>

--- src/components/Marketplace/Lead.js
+++ src/components/Marketplace/Lead.js
@@ -57,6 +57,7 @@ const Lead = ({ lead, user }) => {
               <img src="./unknown.svg" alt="unknown" />
             )}
           </td>
+          <td>{lead.isOpen == true ? <p>Ouvert</p> : <p>?</p>}</td>
           <td className="disable">{dateParser(lead.createdAt)}</td>
           <td>{lead?.buyer?.length}</td>
           <td>
