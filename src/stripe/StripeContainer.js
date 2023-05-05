e7c997ccb306458802e4e4803ed06b3934394854
ClémentL <clement.lidar@gmail.com>
2023-05-06 00:40:55 +0200

    stripe Options

--- src/stripe/StripeContainer.js
+++ src/stripe/StripeContainer.js
@@ -12,14 +12,15 @@ const Stripe = ({
   paymentSuccessful,
   paymentFailed,
 }) => {
-  // const options = {
-  //   // passing the client secret obtained from the server
-  //   clientSecret: "{{CLIENT_SECRET}}",
-  // };
+  const options = {
+    // passing the client secret obtained from the server
+    clientSecret:
+      "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",
+  };
   return (
     <Elements
       stripe={stripeTestPromise}
-      // options={options}
+      options={options}
       closeCheckoutForm={closeCheckoutForm}
       plan={plan}
     >
