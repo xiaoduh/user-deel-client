e0aba191bb84f8393481a688bf4151d316d2163a
ClémentL <clement.lidar@gmail.com>
2023-05-17 00:21:09 +0200

    fdp

--- src/components/MyLeads/BusinessProvider.js
+++ src/components/MyLeads/BusinessProvider.js
@@ -75,7 +75,7 @@ const Sales = () => {
     const errorCompanyRequired = document.querySelector(".company-required");
     const errorSkillRequired = document.querySelector(".skill-required");
     const errorRegionRequired = document.querySelector(".region-required");
-    const errorFdpRequired = document.querySelector(".region-required");
+    const errorFdpRequired = document.querySelector(".fdp-required");
     e.preventDefault();
 
     if (profil === null || profil === "") {
