a2b24fbe073edbb276eea165873b8a4416544f9a
clement <clement.lidar@gmail.com>
2023-05-21 23:26:37 +0200

    spinner loading when confirme buy

--- src/components/Marketplace/Popup.js
+++ src/components/Marketplace/Popup.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { NavLink } from "react-router-dom";
 import { getLeads } from "../../actions/leads.actions";
