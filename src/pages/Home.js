f7e1776bf17bd24a257c6739739d80b7637ba416
clement <clement.lidar@gmail.com>
2023-07-04 12:38:01 +0200

    loader annonces

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,15 +2,22 @@ import React, { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 import ReactGA from "react-ga";
 import Annonces from "../components/Annonces/Annonces";
+import { useSelector } from "react-redux";
 
 const Home = () => {
   const [isPosting, setIsPosting] = useState(false);
   const [popupDiscussions, setPopupDiscussions] = useState(false);
   const [popupGain, setPopupGain] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const annoncesData = useSelector((state) => state.annoncesReducer);
 
   useEffect(() => {
     ReactGA.pageview(window.location.pathname);
   }, []);
+
+  useEffect(() => {
+    annoncesData[1] && setLoading(false);
+  }, [loading, annoncesData]);
 
   const openIsPosting = () => {
     setIsPosting(true);
@@ -44,14 +51,35 @@ const Home = () => {
           openDiscussions={openDiscussions}
           openGain={openGain}
         />
-        <Annonces
-          isPosting={isPosting}
-          closeIsPosting={closeIsPosting}
-          popupDiscussions={popupDiscussions}
-          closeDiscussions={closeDiscussions}
-          popupGain={popupGain}
-          closeGain={closeGain}
-        />
+        {loading ? (
+          <main style={{ width: "100%" }}>
+            <div
+              className="loading-container"
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
+                width: "100%",
+              }}
+            >
+              <p>Chragement des annonces</p>
+              <div>
+                {" "}
+                <i className="fas fa-spinner fa-spin loading"></i>
+              </div>
+            </div>
+          </main>
+        ) : (
+          <Annonces
+            isPosting={isPosting}
+            closeIsPosting={closeIsPosting}
+            popupDiscussions={popupDiscussions}
+            closeDiscussions={closeDiscussions}
+            popupGain={popupGain}
+            closeGain={closeGain}
+          />
+        )}
       </div>
     </div>
   );
